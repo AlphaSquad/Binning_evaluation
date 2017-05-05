8fcc6e4187f2c55f3a8f3694898b6a591dfea15f
Mark Mammel <msmammel@verizon.net>
2017-05-05 12:23:43 -0400

    update

--- README.md
+++ README.md
@@ -15,3 +15,48 @@ Input:
 
 Output:
 * NCBI taxdump including all the taxids appearing at least once in any of the input binning files
+
+
+###confusionmatrix.py
+Script for reading the ground truth for each sequence id and the participant assignment for each seqid
+
+Format of ground truth file
+Name = gsa.tsa
+Format = tab separated text
+Example:
+#SequenceID	#TaxID	#Length
+Contig_1	2	1000
+The third column (length) is optional. If absent, each length will be set to 1.
+
+Format of participant file
+Name = participant.tsa
+Format = tab separated text
+Example:
+#SequenceID	#TaxID
+Contig_1	2
+
+Format of output file
+Name = result_all.tsa
+Format = tab separated text
+Example:
+#tax_id	tp	fp	tn	fn
+200666	1	0	52	0
+
+If the -rank argument is added to the command, the program will read in the taxonomy
+and summarize the results for each level of taxonomy.
+
+Format of output file by rank
+Name = result_rank.tsa
+Format = tab separated text
+Example:
+#	tp	fp	tn	fn
+species	6	2	466	3
+#	tp	fp	tn	fn
+total	196	30	2536	47
+
+tp = true positive -- the participant reports a taxid for a sequence which matches the ground truth at that level of taxonomy.
+fp = false positive -- the participant reports a taxid for a sequence which does not match the ground truth at that level of taxonomy 
+AND the ground truth DOES report a result for that level of taxonomy.
+For example if participant reports Escherichia coli and the ground truth for the sequence is Escherichia, participant gets tp for genus and no result for species.
+tn = true negative -- participant does not report a particular taxid for a sequence and neither does ground truth.
+fn = false negative -- participant does not report a particular taxid for a sequence but the ground truth does.
--- confusionmatrix.py
+++ confusionmatrix.py
@@ -1,15 +1,32 @@
 #!/usr/bin/env python
-from sys import argv, stdout, stderr, stdin, exit
+import sys
+import re
+import argparse
 
 #program to read in ground truth file and participant file
+#will read in taxonomy tree if -rank in command
 #report confusion matrix as tsv
-#TODO sort output, report per taxonomy rank
+#TODO read length file
 
-input_file1 = "gsa.tsv"
-input_file2 = "participant.tsv"
-output_file = "result.tsv"
+#ground truth input file
+#input_file1 = "./tests/minimal_gsa_with_lengths.tsv"
+input_file1 = "./tests/minimal_gs.tsv"
+#participant responses
+input_file2 = "./tests/minimal_participant.tsv"
+#NCBI taxonomy
+input_file3 = "./tests/minimal_taxonomy.tsv"
+#output of results
+output_file1 = "./tests/minimal_result_all.tsv"
+output_file2 = "./tests/minimal_result_rank.tsv"
+
+if __name__ == '__main__':
+    rank_on = False #combine tax_id by taxonomic rank
+    if len(sys.argv) > 1:
+        if sys.argv[1].strip() == '-rank':
+            rank_on = True
 
 tax_set = set()
+tax_set2 = set() #including all levels of taxonomy
 seq_set = set()
 #ground truth assignments
 gsa_dict = dict()
@@ -58,7 +75,39 @@ with open(input_file2, "r") as f:
                 if len(rows) > 1:
                     seq_id, tax_id = rows[:2]
                     partic_dict[seq_id] = tax_id
+                    tax_set.add(tax_id)
 f.close()
+
+rank_set = dict() #dictionary of sets of all taxonomy numbers associated with a tax_id
+rank_cat = dict() #rank that this tax_id belongs to
+rank_idx = dict() #index of rank in list
+rank_list = []  #ranks seen
+
+#open taxonomy file
+if rank_on:
+    with open(input_file3, "r") as f:
+        for line in f:
+            if len(line) > 1:
+                if line[0] == '#':
+                    j = 0 #for later use?
+                elif line[0] == '@':
+                    j = 0 #for later use?
+                else:
+                    rows = line.strip().split("\t")
+                    if len(rows) > 1:
+                        tax_id = rows[0]
+                        rank_set[tax_id] = set()
+                        for j in range(1, len(rows) - 2, 3):
+                            rank = rows[j]
+                            rank_id = rows[j+1]
+                            rank_name = rows[j+2] #not saved
+                            if rank not in rank_idx:
+                                rank_idx[rank] = len(rank_list)
+                                rank_list.append(rank)
+                            rank_cat[tax_id] = rank_idx[rank]
+                            rank_set[tax_id].add(rank_id)
+                            tax_set2.add(rank_id)
+    f.close()
 
 #check for any Seq id not identified by participant
 #assign the id to -1
@@ -66,57 +115,100 @@ num_not_assigned = 0
 for seq_id in seq_set:
     if seq_id not in partic_dict:
         num_not_assigned += 1
-        partic_dict[seq_id] = -1
+        partic_dict[seq_id] = "none"
+rank_cat["none"] = 0
+rank_set["none"] = set()
         
-print num_not_assigned, "not assigned by participant"
+#print num_not_assigned, "not assigned by participant"
 
-#totals
-false_pos = 0
-false_neg = 0
-true_pos = 0
-true_neg = 0
+result_file1 = open(output_file1, 'w')
+line = "# " + str(num_not_assigned) + " not assigned by participant\n"
+result_file1.write(line)
 
+#total stats
+total_stats = [0,0,0,0]
 #per tax_id:  tp, fp, tn, fn 
-stats = dict()
+tax_stats = dict()
 
 for tax_id in tax_set:
-    stats[tax_id] = [0,0,0,0]
-
-    #check if participant's assigned to this taxid is correct
+    tax_stats[tax_id] = [0,0,0,0]     
+    #check if participant's assignment to this taxid is correct
     for seq_id in seq_set:
-        if partic_dict[seq_id] == tax_id:
+        if partic_dict[seq_id] == tax_id: #tp
             if gsa_dict[seq_id] == tax_id:
-                stats[tax_id][0] += length_dict[seq_id]
-                true_pos += length_dict[seq_id]
-            else:
-                stats[tax_id][1] += length_dict[seq_id]
-                false_pos += length_dict[seq_id]
-        else:
-            if gsa_dict[seq_id] == tax_id:
-                stats[tax_id][3] += length_dict[seq_id]
-                false_neg += length_dict[seq_id]
-            else:
-                stats[tax_id][2] += length_dict[seq_id]
-                true_neg += length_dict[seq_id]
-
-#output results
-result_file = open(output_file, 'w')
-line = str(num_not_assigned) + "not assigned by participant"
-result_file.write(line)
-line = "total true positive " + str(true_pos) + '\n'
-result_file.write(line)
-line = "total false positive " + str(false_pos) + '\n'
-result_file.write(line)
-line = "true negative " + str(true_neg) + '\n'
-result_file.write(line)
-line = "false negative " + str(false_neg) + '\n'
-result_file.write(line)
-
-line = "tax_id\ttp\tfp\ttn\tfn\n"
-result_file.write(line)
-
-for tax_id in tax_set:
-    line = tax_id + "\t" + "\t".join(stats[taxid]) + "\n"
-    result_file.write(line)
+                tax_stats[tax_id][0] += length_dict[seq_id]
+                total_stats[0] += length_dict[seq_id]
+            else: #fp
+                tax_stats[tax_id][1] += length_dict[seq_id]
+                total_stats[1] += length_dict[seq_id]
+        else: 
+            if gsa_dict[seq_id] == tax_id: #fn
+                tax_stats[tax_id][3] += length_dict[seq_id]
+                total_stats[3] += length_dict[seq_id]
+            else: #tn
+                tax_stats[tax_id][2] += length_dict[seq_id]
+                total_stats[2] += length_dict[seq_id]
     
-result_file.close()
+#output results 
+line = "#tax_id\ttp\tfp\ttn\tfn\n"
+result_file1.write(line)
+for tax_id in tax_set:
+    line = tax_id + "\t" + "\t".join(map(str,tax_stats[tax_id])) + "\n"
+    result_file1.write(line)
+
+line = "total\t" + "\t".join(map(str,total_stats)) + "\n"
+result_file1.write(line)
+result_file1.close()
+
+if rank_on:
+    tax_set = tax_set.union(tax_set2)
+    result_file2 = open(output_file2, 'w')
+    line = "# " + str(num_not_assigned) + " not assigned by participant\n"
+    result_file2.write(line)
+    line =  "#\ttp\tfp\ttn\tfn\n"
+    result_file2.write(line)
+
+    #total stats
+    total_stats = [0,0,0,0]
+    #per tax_id:  tp, fp, tn, fn 
+    tax_stats = dict()
+    
+    for rank_num in range(len(rank_list)):
+        rank_stats = [0,0,0,0]
+        for tax_id in tax_set:
+            if rank_cat[tax_id] == rank_num:
+                tax_stats[tax_id] = [0,0,0,0]     
+                #check if participant's assignment to this taxid is correct
+                for seq_id in seq_set:
+                    if tax_id in rank_set[partic_dict[seq_id]]:
+                        if tax_id in rank_set[gsa_dict[seq_id]]: #tp
+                            rank_stats[0] += length_dict[seq_id]
+                            tax_stats[tax_id][0] += length_dict[seq_id]
+                            total_stats[0] += length_dict[seq_id]
+                        else: #fp
+                            #determine if any call made in ground truth file
+                            called = False
+                            for tid in rank_set[gsa_dict[seq_id]]:
+                                if rank_cat[tid] == rank_num:
+                                    called = True
+                            if called:
+                                rank_stats[1] += length_dict[seq_id]
+                                tax_stats[tax_id][1] += length_dict[seq_id]
+                                total_stats[1] += length_dict[seq_id]
+                    else:
+                        if tax_id in rank_set[gsa_dict[seq_id]]: #fn
+                            rank_stats[3] += length_dict[seq_id]
+                            tax_stats[tax_id][3] += length_dict[seq_id]
+                            total_stats[3] += length_dict[seq_id]
+                        else: #tn
+                            rank_stats[2] += length_dict[seq_id]
+                            tax_stats[tax_id][2] += length_dict[seq_id]
+                            total_stats[2] += length_dict[seq_id]
+            
+        #output results 
+        line = rank_list[rank_num] + "\t" + "\t".join(map(str,rank_stats)) + "\n"
+        result_file2.write(line) 
+    
+    line = "total\t" + "\t".join(map(str,total_stats)) + "\n"
+    result_file2.write(line)
+    result_file2.close()
